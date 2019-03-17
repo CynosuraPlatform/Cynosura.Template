abc671925af1e3a13dbbde0efc87a949a3f03209
Vassily Ivanov <vivanov@aeb-it.ru>
2019-03-17 09:26:53 +0900

    Fix pageIndex and pageSize checks

--- src/Cynosura.Template.Web/ClientApp/src/app/user-core/user.service.ts
+++ src/Cynosura.Template.Web/ClientApp/src/app/user-core/user.service.ts
@@ -16,11 +16,11 @@ export class UserService {
 
         let params = new HttpParams();
 
-        if (pageIndex !== undefined) {
+        if (pageIndex !== undefined && pageIndex !== null) {
             params = params.set("pageIndex", pageIndex.toString());
         }
 
-        if (pageSize !== undefined) {
+        if (pageSize !== undefined && pageSize !== null) {
             params = params.set("pageSize", pageSize.toString());
         }
 
