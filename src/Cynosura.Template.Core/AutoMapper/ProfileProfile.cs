7a86e6f5a3508c8198a32e3a42675969b4b99d03
Vassily Ivanov <vivanov@aeb-it.ru>
2019-10-24 11:10:50 +0900

    Fix profile page

---
+++ src/Cynosura.Template.Core/AutoMapper/ProfileProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Cynosura.Template.Core.Entities;
+using Cynosura.Template.Core.Requests.Profile.Models;
+
+namespace Cynosura.Template.Core.AutoMapper
+{
+    public class ProfileProfile : Profile
+    {
+        public ProfileProfile()
+        {
+            CreateMap<User, ProfileModel>();
+        }
+    }
+}
