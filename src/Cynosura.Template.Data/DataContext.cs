ae2f346cef50423799bf91bd511bfb99e271c279
Vassily Ivanov <bacr@bacr.ru>
2019-11-04 14:51:42 +0900

    Add Identity Server (draft)

--- src/Cynosura.Template.Data/DataContext.cs
+++ src/Cynosura.Template.Data/DataContext.cs
@@ -5,20 +5,32 @@ using System.Threading.Tasks;
 using Cynosura.EF;
 using Cynosura.Template.Core.Entities;
 using Cynosura.Template.Core.Infrastructure;
+using IdentityServer4.EntityFramework.Entities;
+using IdentityServer4.EntityFramework.Extensions;
+using IdentityServer4.EntityFramework.Interfaces;
+using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace Cynosura.Template.Data
 {
-    public class DataContext : IdentityDbContext<User, Role, int>
+    public class DataContext : IdentityDbContext<User, Role, int>, IPersistedGrantDbContext
     {
+        private readonly IOptions<OperationalStoreOptions> _operationalStoreOptions;
+
         public event EventHandler SavingChanges;
 
-        public DataContext(DbContextOptions<DataContext> options)
+        public DataContext(DbContextOptions<DataContext> options, 
+            IOptions<OperationalStoreOptions> operationalStoreOptions)
             : base(options)
         {
-            
+            _operationalStoreOptions = operationalStoreOptions;
         }
+
+        public DbSet<PersistedGrant> PersistedGrants { get; set; }
+
+        public DbSet<DeviceFlowCodes> DeviceFlowCodes { get; set; }
 
         public override int SaveChanges()
         {
@@ -35,6 +47,7 @@ namespace Cynosura.Template.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            builder.ConfigurePersistedGrantContext(_operationalStoreOptions.Value);
 
             var assemblies = CoreHelper.GetPlatformAndAppAssemblies();
             builder.ApplyAllConfigurations(assemblies);
@@ -44,5 +57,7 @@ namespace Cynosura.Template.Data
         {
             SavingChanges?.Invoke(this, EventArgs.Empty);
         }
+
+        Task<int> IPersistedGrantDbContext.SaveChangesAsync() => SaveChangesAsync();
     }
 }
