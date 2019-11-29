2fa023dadff7ba1970238d629437933c886885a3
Vassily Ivanov <vivanov@sharplogic.ru>
2019-11-29 15:51:11 +0900

    Remove NLog

--- src/Cynosura.Template.Web/Program.cs
+++ src/Cynosura.Template.Web/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NLog.Web;
 
 namespace Cynosura.Template.Web
 {
@@ -17,27 +16,14 @@ namespace Cynosura.Template.Web
     {
         public static void Main(string[] args)
         {
-            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-            try
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
             {
-                var host = CreateWebHostBuilder(args).Build();
-                using (var scope = host.Services.CreateScope())
-                {
-                    var services = scope.ServiceProvider;
-                    var databaseInitializer = (IDatabaseInitializer)services.GetService(typeof(IDatabaseInitializer));
-                    databaseInitializer.SeedAsync().GetAwaiter().GetResult();
-                }
-                host.Run();
+                var services = scope.ServiceProvider;
+                var databaseInitializer = (IDatabaseInitializer)services.GetService(typeof(IDatabaseInitializer));
+                databaseInitializer.SeedAsync().GetAwaiter().GetResult();
             }
-            catch (Exception ex)
-            {
-                logger.Error(ex, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                NLog.LogManager.Shutdown();
-            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
@@ -46,8 +32,10 @@ namespace Cynosura.Template.Web
                 .ConfigureLogging(logging =>
                 {
                     logging.ClearProviders();
-                    logging.SetMinimumLevel(LogLevel.Trace);
-                })
-                .UseNLog();
+                    logging.AddConsole(c =>
+                    {
+                        c.TimestampFormat = "[yyyy-MM-dd HH:mm:ss] ";
+                    });
+                });
     }
 }
