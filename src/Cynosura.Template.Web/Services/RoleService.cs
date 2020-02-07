6fc54c8dbdbe3fc5de2c3e1c277d56d7915c55dc
Vassily Ivanov <vivanov@sharplogic.ru>
2020-02-07 10:43:53 +0900

    Add Grpc authorization

--- src/Cynosura.Template.Web/Services/RoleService.cs
+++ src/Cynosura.Template.Web/Services/RoleService.cs
@@ -10,9 +10,11 @@ using Cynosura.Template.Core.Requests.Roles.Models;
 using Cynosura.Template.Web.Protos;
 using Grpc.Core;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Cynosura.Template.Web.Services
 {
+    [Authorize("ReadRole")]
     public class RoleService : Roles.RolesBase
     {
         private readonly IMediator _mediator;
@@ -24,34 +26,37 @@ namespace Cynosura.Template.Web.Services
             _mapper = mapper;
         }
 
-        public override async Task<CreatedEntity> CreateRole(CreateRoleRequest createRoleRequest, ServerCallContext context)
-        {
-            var createRole = _mapper.Map<CreateRoleRequest, CreateRole>(createRoleRequest);
-            return _mapper.Map<CreatedEntity<int>, CreatedEntity>(await _mediator.Send(createRole));
-        }
-
-        public override async Task<Empty> DeleteRole(DeleteRoleRequest deleteRoleRequest, ServerCallContext context)
-        {
-            var deleteRole = _mapper.Map<DeleteRoleRequest, DeleteRole>(deleteRoleRequest);
-            return _mapper.Map<Unit, Empty>(await _mediator.Send(deleteRole));
-        }
-
-        public override async Task<Role> GetRole(GetRoleRequest getRoleRequest, ServerCallContext context)
-        {
-            var getRole = _mapper.Map<GetRoleRequest, GetRole>(getRoleRequest);  
-            return _mapper.Map<RoleModel, Role>(await _mediator.Send(getRole));
-        }
-
         public override async Task<RolePageModel> GetRoles(GetRolesRequest getRolesRequest, ServerCallContext context)
         {
             var getRoles = _mapper.Map<GetRolesRequest, GetRoles>(getRolesRequest);
             return _mapper.Map<PageModel<RoleModel>, RolePageModel>(await _mediator.Send(getRoles));
         }
 
+        public override async Task<Role> GetRole(GetRoleRequest getRoleRequest, ServerCallContext context)
+        {
+            var getRole = _mapper.Map<GetRoleRequest, GetRole>(getRoleRequest);
+            return _mapper.Map<RoleModel, Role>(await _mediator.Send(getRole));
+        }
+
+        [Authorize("WriteRole")]
         public override async Task<Empty> UpdateRole(UpdateRoleRequest updateRoleRequest, ServerCallContext context)
         {
             var updateRole = _mapper.Map<UpdateRoleRequest, UpdateRole>(updateRoleRequest);
             return _mapper.Map<Unit, Empty>(await _mediator.Send(updateRole));
+        }
+
+        [Authorize("WriteRole")]
+        public override async Task<CreatedEntity> CreateRole(CreateRoleRequest createRoleRequest, ServerCallContext context)
+        {
+            var createRole = _mapper.Map<CreateRoleRequest, CreateRole>(createRoleRequest);
+            return _mapper.Map<CreatedEntity<int>, CreatedEntity>(await _mediator.Send(createRole));
+        }        
+
+        [Authorize("WriteRole")]
+        public override async Task<Empty> DeleteRole(DeleteRoleRequest deleteRoleRequest, ServerCallContext context)
+        {
+            var deleteRole = _mapper.Map<DeleteRoleRequest, DeleteRole>(deleteRoleRequest);
+            return _mapper.Map<Unit, Empty>(await _mediator.Send(deleteRole));
         }
     }
 }
--- src/Cynosura.Template.Web/Services/UserService.cs
+++ src/Cynosura.Template.Web/Services/UserService.cs
@@ -10,9 +10,11 @@ using Cynosura.Template.Core.Requests.Users.Models;
 using Cynosura.Template.Web.Protos;
 using Grpc.Core;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Cynosura.Template.Web.Services
 {
+    [Authorize("ReadUser")]
     public class UserService : Users.UsersBase
     {
         private readonly IMediator _mediator;
@@ -24,34 +26,37 @@ namespace Cynosura.Template.Web.Services
             _mapper = mapper;
         }
 
-        public override async Task<CreatedEntity> CreateUser(CreateUserRequest createUserRequest, ServerCallContext context)
-        {
-            var createUser = _mapper.Map<CreateUserRequest, CreateUser>(createUserRequest);
-            return _mapper.Map<CreatedEntity<int>, CreatedEntity>(await _mediator.Send(createUser));
-        }
-
-        public override async Task<Empty> DeleteUser(DeleteUserRequest deleteUserRequest, ServerCallContext context)
-        {
-            var deleteUser = _mapper.Map<DeleteUserRequest, DeleteUser>(deleteUserRequest);
-            return _mapper.Map<Unit, Empty>(await _mediator.Send(deleteUser));
-        }
-
-        public override async Task<User> GetUser(GetUserRequest getUserRequest, ServerCallContext context)
-        {
-            var getUser = _mapper.Map<GetUserRequest, GetUser>(getUserRequest);  
-            return _mapper.Map<UserModel, User>(await _mediator.Send(getUser));
-        }
-
         public override async Task<UserPageModel> GetUsers(GetUsersRequest getUsersRequest, ServerCallContext context)
         {
             var getUsers = _mapper.Map<GetUsersRequest, GetUsers>(getUsersRequest);
             return _mapper.Map<PageModel<UserModel>, UserPageModel>(await _mediator.Send(getUsers));
         }
 
+        public override async Task<User> GetUser(GetUserRequest getUserRequest, ServerCallContext context)
+        {
+            var getUser = _mapper.Map<GetUserRequest, GetUser>(getUserRequest);
+            return _mapper.Map<UserModel, User>(await _mediator.Send(getUser));
+        }
+
+        [Authorize("WriteUser")]
         public override async Task<Empty> UpdateUser(UpdateUserRequest updateUserRequest, ServerCallContext context)
         {
             var updateUser = _mapper.Map<UpdateUserRequest, UpdateUser>(updateUserRequest);
             return _mapper.Map<Unit, Empty>(await _mediator.Send(updateUser));
+        }
+
+        [Authorize("WriteUser")]
+        public override async Task<CreatedEntity> CreateUser(CreateUserRequest createUserRequest, ServerCallContext context)
+        {
+            var createUser = _mapper.Map<CreateUserRequest, CreateUser>(createUserRequest);
+            return _mapper.Map<CreatedEntity<int>, CreatedEntity>(await _mediator.Send(createUser));
+        }
+
+        [Authorize("WriteUser")]
+        public override async Task<Empty> DeleteUser(DeleteUserRequest deleteUserRequest, ServerCallContext context)
+        {
+            var deleteUser = _mapper.Map<DeleteUserRequest, DeleteUser>(deleteUserRequest);
+            return _mapper.Map<Unit, Empty>(await _mediator.Send(deleteUser));
         }
     }
 }
