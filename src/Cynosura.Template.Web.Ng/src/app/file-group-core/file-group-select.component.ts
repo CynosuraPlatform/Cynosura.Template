c6ff4775fb5af863a069840c322bfd3c0b63afab
Vassily Ivanov <vivanov@sharplogic.ru>
2020-05-26 15:37:04 +0900

    Add select autocomplete mode

--- src/Cynosura.Template.Web.Ng/src/app/file-group-core/file-group-select.component.ts
+++ src/Cynosura.Template.Web.Ng/src/app/file-group-core/file-group-select.component.ts
@@ -1,9 +1,10 @@
 ﻿import { Component, Input, OnInit, forwardRef, OnDestroy, ElementRef, Optional, Self, DoCheck } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl } from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl, FormControl } from '@angular/forms';
 import { MatFormFieldControl } from '@angular/material/form-field';
 import { FocusMonitor } from '@angular/cdk/a11y';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import { Subject } from 'rxjs';
+import { Subject, Observable, of } from 'rxjs';
+import { filter, debounceTime, mergeMap, map, startWith } from 'rxjs/operators';
 
 import { FileGroup } from './file-group.model';
 import { FileGroupService } from './file-group.service';
@@ -33,11 +34,15 @@ export class FileGroupSelectComponent implements OnInit, ControlValueAccessor,
         return !this.value;
     }
 
-    get shouldLabelFloat() { return this.focused || !this.empty; }
+    get shouldLabelFloat() { return this.focused || !this.empty || this.autocompleteControl.value; }
 
     FileGroup = FileGroup;
 
     fileGroups: FileGroup[] = [];
+
+    autocompleteFileGroups: Observable<FileGroup[]>;
+
+    autocompleteControl = new FormControl();
 
     @Input()
     value: number | null = null;
@@ -57,9 +62,17 @@ export class FileGroupSelectComponent implements OnInit, ControlValueAccessor,
     private innerRequired = false;
 
     @Input()
+    mode: 'select' | 'autocomplete' = 'select';
+
+    @Input()
     get disabled(): boolean { return this.innerDisabled; }
     set disabled(value: boolean) {
         this.innerDisabled = coerceBooleanProperty(value);
+        if (value) {
+            this.autocompleteControl.disable();
+        } else {
+            this.autocompleteControl.enable();
+        }
         this.stateChanges.next();
     }
     private innerDisabled = false;
@@ -102,7 +115,38 @@ export class FileGroupSelectComponent implements OnInit, ControlValueAccessor,
     }
 
     ngOnInit(): void {
-        this.fileGroupService.getFileGroups({}).subscribe(fileGroups => this.fileGroups = fileGroups.pageItems);
+        if (this.mode === 'select') {
+            this.fileGroupService.getFileGroups({}).subscribe(fileGroups => this.fileGroups = fileGroups.pageItems);
+        } else if (this.mode === 'autocomplete') {
+            this.autocompleteFileGroups = this.autocompleteControl.valueChanges
+                .pipe(
+                    map(value => {
+                        if (value === '') {
+                            this.innerValue = null;
+                            return value;
+                        } else if (typeof value === 'string') {
+                            return value;
+                        } else {
+                            this.innerValue = value.id;
+                            return this.getDisplay(value);
+                        }
+                    }),
+                    filter(val => val.length > 2 || val.length === 0),
+                    debounceTime(500),
+                    mergeMap(val => {
+                        if (val.length !== 0) {
+                            return this.fileGroupService.getFileGroups({
+                                filter: { text: val }
+                            }).pipe(map(res => res.pageItems));
+                        } else {
+                            return of(<FileGroup[]>[]);
+                        }
+                    }));
+            if (this.value) {
+                this.fileGroupService.getFileGroup({ id: this.value })
+                    .subscribe(fileGroup => this.autocompleteControl.setValue(fileGroup));
+            }
+        }
     }
 
     ngOnDestroy() {
@@ -122,5 +166,9 @@ export class FileGroupSelectComponent implements OnInit, ControlValueAccessor,
             this.errorState = this.ngControl.invalid;
             this.stateChanges.next();
         }
+    }
+
+    getDisplay(fileGroup: FileGroup) {
+        return fileGroup ? fileGroup.name : '';
     }
 }
--- src/Cynosura.Template.Web.Ng/src/app/role-core/role-select.component.ts
+++ src/Cynosura.Template.Web.Ng/src/app/role-core/role-select.component.ts
@@ -1,9 +1,10 @@
 ﻿import { Component, Input, OnInit, forwardRef, OnDestroy, ElementRef, Optional, Self, DoCheck } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl } from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl, FormControl } from '@angular/forms';
 import { MatFormFieldControl } from '@angular/material/form-field';
 import { FocusMonitor } from '@angular/cdk/a11y';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import { Subject } from 'rxjs';
+import { Subject, Observable, of } from 'rxjs';
+import { filter, debounceTime, mergeMap, map, startWith } from 'rxjs/operators';
 
 import { Role } from './role.model';
 import { RoleService } from './role.service';
@@ -33,11 +34,15 @@ export class RoleSelectComponent implements OnInit, ControlValueAccessor,
         return !this.value;
     }
 
-    get shouldLabelFloat() { return this.focused || !this.empty; }
+    get shouldLabelFloat() { return this.focused || !this.empty || this.autocompleteControl.value; }
 
     Role = Role;
 
     roles: Role[] = [];
+
+    autocompleteRoles: Observable<Role[]>;
+
+    autocompleteControl = new FormControl();
 
     @Input()
     value: number | null = null;
@@ -57,9 +62,17 @@ export class RoleSelectComponent implements OnInit, ControlValueAccessor,
     private innerRequired = false;
 
     @Input()
+    mode: 'select' | 'autocomplete' = 'select';
+
+    @Input()
     get disabled(): boolean { return this.innerDisabled; }
     set disabled(value: boolean) {
         this.innerDisabled = coerceBooleanProperty(value);
+        if (value) {
+            this.autocompleteControl.disable();
+        } else {
+            this.autocompleteControl.enable();
+        }
         this.stateChanges.next();
     }
     private innerDisabled = false;
@@ -102,7 +115,38 @@ export class RoleSelectComponent implements OnInit, ControlValueAccessor,
     }
 
     ngOnInit(): void {
-        this.roleService.getRoles({}).subscribe(roles => this.roles = roles.pageItems);
+        if (this.mode === 'select') {
+            this.roleService.getRoles({}).subscribe(roles => this.roles = roles.pageItems);
+        } else if (this.mode === 'autocomplete') {
+            this.autocompleteRoles = this.autocompleteControl.valueChanges
+                .pipe(
+                    map(value => {
+                        if (value === '') {
+                            this.innerValue = null;
+                            return value;
+                        } else if (typeof value === 'string') {
+                            return value;
+                        } else {
+                            this.innerValue = value.id;
+                            return this.getDisplay(value);
+                        }
+                    }),
+                    filter(val => val.length > 2 || val.length === 0),
+                    debounceTime(500),
+                    mergeMap(val => {
+                        if (val.length !== 0) {
+                            return this.roleService.getRoles({
+                                filter: { text: val }
+                            }).pipe(map(res => res.pageItems));
+                        } else {
+                            return of(<Role[]>[]);
+                        }
+                    }));
+            if (this.value) {
+                this.roleService.getRole({ id: this.value })
+                    .subscribe(role => this.autocompleteControl.setValue(role));
+            }
+        }
     }
 
     ngOnDestroy() {
@@ -122,5 +166,9 @@ export class RoleSelectComponent implements OnInit, ControlValueAccessor,
             this.errorState = this.ngControl.invalid;
             this.stateChanges.next();
         }
+    }
+
+    getDisplay(role: Role) {
+        return role ? role.name : '';
     }
 }
