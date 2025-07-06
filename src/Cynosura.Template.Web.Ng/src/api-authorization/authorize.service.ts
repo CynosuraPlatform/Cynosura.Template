299bc77f2cb59f098e79fd549e26c21178ee6169
Vassily Ivanov <vivanov@sharplogic.ru>
2025-07-06 23:48:50 +0900

    Revert authorize changes

--- src/Cynosura.Template.Web.Ng/src/api-authorization/authorize.service.ts
+++ src/Cynosura.Template.Web.Ng/src/api-authorization/authorize.service.ts
@@ -94,11 +94,7 @@ export class AuthorizeService {
     let user: User = null;
     try {
       user = await this.userManager.signinSilent(this.createArguments());
-      this.userSubject.next({
-        ...user.profile,
-        name: user.profile.name ?? "",
-        role: user.profile.role ?? ""
-      });
+      this.userSubject.next(user.profile);
       return this.success(state);
     } catch (silentError) {
       // User might not be authenticated, fallback to popup authentication
@@ -109,11 +105,7 @@ export class AuthorizeService {
           throw new Error('Popup disabled. Change \'authorize.service.ts:AuthorizeService.popupDisabled\' to false to enable it.');
         }
         user = await this.userManager.signinPopup(this.createArguments());
-        this.userSubject.next({
-          ...user.profile,
-          name: user.profile.name ?? "",
-          role: user.profile.role ?? ""
-        });
+        this.userSubject.next(user.profile);
         return this.success(state);
       } catch (popupError) {
         if (popupError.message === 'Popup window closed') {
@@ -139,11 +131,7 @@ export class AuthorizeService {
     try {
       await this.ensureUserManagerInitialized();
       const user = await this.userManager.signinCallback(url);
-      this.userSubject.next(user && {
-        ...user.profile,
-        name: user.profile.name ?? "",
-        role: user.profile.role ?? ""
-      });
+      this.userSubject.next(user && user.profile);
       return this.success(user && user.state);
     } catch (error) {
       console.log('There was an error signing in: ', error);
@@ -226,10 +214,6 @@ export class AuthorizeService {
     return from(this.ensureUserManagerInitialized())
       .pipe(
         mergeMap(() => this.userManager.getUser()),
-        map(u => u && {
-          ...u.profile,
-          name: u.profile.name ?? "",
-          role: u.profile.role ?? ""
-        }));
+        map(u => u && u.profile));
   }
 }
