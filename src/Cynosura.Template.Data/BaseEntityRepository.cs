d0364716493e62d198201fc1001929c53620f922
Vassily Ivanov <vivanov@sharplogic.ru>
2025-07-13 22:50:16 +0900

    Fix change tracking (do not track wrong entities)

--- src/Cynosura.Template.Data/BaseEntityRepository.cs
+++ src/Cynosura.Template.Data/BaseEntityRepository.cs
@@ -64,7 +64,10 @@ namespace Cynosura.Template.Data
             {
                 foreach (var entry in e.AddedEntities)
                 {
-                    TrackChange(entry, ChangeAction.Add);
+                    if (entry.Entity is T)
+                    {
+                        TrackChange(entry, ChangeAction.Add);
+                    }
                 }
             }
         }
