aa1be2ebdfaac16ea89feda78ac710b707fea74b
Vassily Ivanov <vivanov@sharplogic.ru>
2026-02-06 09:59:29 +0900

    Fix Swagger gen: use full type name for schema id

--- src/Cynosura.Template.Web/Startup.cs
+++ src/Cynosura.Template.Web/Startup.cs
@@ -108,6 +108,7 @@ namespace Cynosura.Template.Web
                     }
                 });
                 c.OperationFilter<SecurityRequirementsOperationFilter>();
+                c.CustomSchemaIds(type => type.ToString());
                 c.SchemaFilter<EnumTypesSchemaFilter>();
                 c.SupportNonNullableReferenceTypes();
             });
